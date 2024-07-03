543325a2f2ddcc5f94448d1dac38a3b8fdb56246
FredoPro <iamalfredo@outlook.pt>
2024-07-03 23:09:36 +0100

    provider erro resolved

--- src/TokenServiceProvider.php
+++ src/TokenServiceProvider.php
@@ -3,42 +3,29 @@
 namespace FredoAntonio\License;
 
 use FredoAntonio\License\Http\Middleware\CheckTokenMiddleware;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Config;
+
 class LicenseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         // Publicar arquivo de configuração
-     /*    $this->publishes([
-            __DIR__ . '/../config/tokendata.php' => __DIR__ . '/../../config/tokendata.php',
-        ], 'config'); */
-        
         $this->publishes([
-            __DIR__ . '/../config/tokendata.php' => config::path('tokendata.php'),
+            __DIR__ . '/../config/tokendata.php' => $this->app->basePath('config/tokendata.php'),
         ], 'config');
 
         // Publicar o middleware
         $this->publishes([
-            __DIR__ . '/../Http/Middleware/CheckTokenMiddleware.php' => App::path('Http/Middleware/CheckTokenMiddleware.php'),
+            __DIR__ . '/../Http/Middleware/CheckTokenMiddleware.php' => $this->app->basePath('app/Http/Middleware/CheckTokenMiddleware.php'),
         ], 'middleware');
-
-        // Publicar o middleware
-      /*   $this->publishes([
-            __DIR__ . '/Http/Middleware/CheckTokenLicense.php' => __DIR__ . 'app/Http/Middleware/CheckTokenLicense.php',
-        ], 'middleware'); */
 
         // Registra o middleware
         $this->app->make('Illuminate\Contracts\Http\Kernel')->pushMiddleware(CheckTokenMiddleware::class);
 
-
+        // Publicar os modelos
         $this->publishes([
-            __DIR__.'/Models' => App::path('Models'),
+            __DIR__ . '/Models' => $this->app->basePath('app/Models'),
         ], 'models');
-       /*  $this->publishes([
-            __DIR__.'/Models' => __DIR__ .'app/Models',
-        ], 'models'); */
     }
 
     public function register()
